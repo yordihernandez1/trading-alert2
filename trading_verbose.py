8d4275c2983e2fda10a413051dcfbf0b6ead4bba
yordihernandez1 <yordi.hernandez.sunset@gmail.com>
2025-05-25 21:10:30 +0200

    Update trading_verbose.py

--- trading_verbose.py
+++ trading_verbose.py
@@ -274,5 +274,5 @@ Con base en los siguientes datos técnicos para {mejor['ticker']}:
 ¿Consideras que es una buena oportunidad para entrar en {'compra' if tipo == 'largo' else 'venta'}? Justifica brevemente.
 '''
 
-        print("🚀 Enviando mensaje a Telegram...")
+            print("🚀 Enviando mensaje a Telegram...")
 enviar_mensaje_telegram(BOT_TOKEN, CHAT_ID, mensaje)
