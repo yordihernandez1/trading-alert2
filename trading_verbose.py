952c6ef8482fee067af71f8972394b671bf496a7
yordihernandez1 <yordi.hernandez.sunset@gmail.com>
2025-05-25 21:16:06 +0200

    Update trading_verbose.py

--- trading_verbose.py
+++ trading_verbose.py
@@ -241,7 +241,7 @@ else:
     )
 
     print("🧠 Generando mensaje de alerta...")
-mensaje = f'''
+    mensaje = f'''
 OPORTUNIDAD DESTACADA: {mejor['ticker']}
 Precio: {mejor['precio']} USD
 RSI: {mejor['rsi']}
@@ -274,5 +274,5 @@ Con base en los siguientes datos técnicos para {mejor['ticker']}:
 ¿Consideras que es una buena oportunidad para entrar en {'compra' if tipo == 'largo' else 'venta'}? Justifica brevemente.
 '''
 
-            print("🚀 Enviando mensaje a Telegram...")
-enviar_mensaje_telegram(BOT_TOKEN, CHAT_ID, mensaje)
+    print("🚀 Enviando mensaje a Telegram...")
+    enviar_mensaje_telegram(BOT_TOKEN, CHAT_ID, mensaje)
